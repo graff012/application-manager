75c6b05b18932fdf2ec9ddd812455d308ecc0f00
Nuriddin Sobirjonov <sobirjonovnuriddin43@gmail.com>
2025-11-26 16:49:30 +0500

    done

--- src/main.ts
+++ src/main.ts
@@ -33,6 +33,7 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('docs', app, document);
 
-  await app.listen(process.env.PORT ?? 3000);
+  const PORT = process.env.PORT || 3000;
+  await app.listen(PORT, '0.0.0.');
 }
 bootstrap();
