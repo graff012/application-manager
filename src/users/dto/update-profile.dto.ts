8be21b615dca7693b428e20bc4f1eb37b9f1c3c6
Nuriddin Sobirjonov <sobirjonovnuriddin43@gmail.com>
2025-12-05 18:36:49 +0500

    added more fields to user update

--- src/users/dto/update-profile.dto.ts
+++ src/users/dto/update-profile.dto.ts
@@ -1,17 +1,48 @@
-import { ApiProperty } from '@nestjs/swagger';
-import { IsEnum, IsOptional, IsString, Matches } from 'class-validator';
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
+import {
+  IsEnum,
+  IsOptional,
+  IsString,
+  Matches,
+  IsMongoId,
+} from 'class-validator';
 
-export class UpdateProfileDto {
-  @ApiProperty({ required: false, example: '+998901234567' })
+export class UpdateUserDto {
+  @ApiPropertyOptional({ example: 'John Doe' })
   @IsOptional()
   @IsString()
-  @Matches(/^\+?[1-9]\d{1,14}$/, {
-    message: 'Phone number must be a valid international format',
+  fullName?: string;
+
+  @ApiPropertyOptional({ example: '+998901234567' })
+  @IsOptional()
+  @IsString()
+  @Matches(/^\+998\d{9}$/, {
+    message: 'Phone number must be valid Uzbekistan number. E.g. +998992224455',
   })
   phone?: string;
 
-  @ApiProperty({ required: false, enum: ['male', 'female', 'other'] })
+  @ApiPropertyOptional({ enum: ['male', 'female', 'other'] })
   @IsOptional()
   @IsEnum(['male', 'female', 'other'])
   gender?: string;
+
+  @ApiPropertyOptional({ enum: ['active', 'blocked'] })
+  @IsOptional()
+  @IsEnum(['active', 'blocked'])
+  status?: 'active' | 'blocked';
+
+  @ApiPropertyOptional()
+  @IsOptional()
+  @IsMongoId()
+  branch?: string;
+
+  @ApiPropertyOptional()
+  @IsOptional()
+  @IsMongoId()
+  department?: string;
+
+  @ApiPropertyOptional()
+  @IsOptional()
+  @IsString()
+  avatar?: string;
 }
