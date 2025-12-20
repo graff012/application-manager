78a8b2172ccf202c8b4cbe701af9c9e03ad3bdf1
Nuriddin Sobirjonov <sobirjonovnuriddin43@gmail.com>
2025-12-20 14:27:56 +0500

    added error handling in tools model

--- src/tools/tools.service.ts
+++ src/tools/tools.service.ts
@@ -1,4 +1,8 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { Tool, ToolDocument } from './schemas/tool.schema';
@@ -11,7 +15,19 @@ export class ToolsService {
     @InjectModel(Tool.name) private readonly toolModel: Model<ToolDocument>,
   ) {}
 
-  create(dto: CreateToolDto) {
+  async create(dto: CreateToolDto) {
+    if (!dto.name || !dto.tagId || !dto.quantity || dto.toolNumber) {
+      throw new BadRequestException(
+        'name, tagId, quantity, toolNumber are required',
+      );
+    }
+
+    const existingByName = await this.toolModel
+      .findOne({ name: dto.name })
+      .exec();
+
+    if (existingByName) throw new BadRequestException('This tool already exists')
+
     return this.toolModel.create(dto);
   }
 
@@ -20,7 +36,10 @@ export class ToolsService {
   }
 
   async findOne(id: string) {
-    const tool = await this.toolModel.findOne({ _id: id }).populate('tagId').exec();
+    const tool = await this.toolModel
+      .findOne({ _id: id })
+      .populate('tagId')
+      .exec();
     if (!tool) throw new NotFoundException('Tool not found');
     return tool;
   }
